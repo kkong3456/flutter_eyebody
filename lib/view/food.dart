7ea14419f61324768c2adde1054cb8c13358e3f2
unknown <kkong3456@gmail.com>
2022-06-03 07:05:15 +0900

    temp-4

--- lib/view/food.dart
+++ lib/view/food.dart
@@ -1,6 +1,8 @@
 import 'package:eyebody/data/data.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:multi_image_picker/multi_image_picker.dart';
 
 class FoodAddPage extends StatefulWidget {
   final Food food;
@@ -15,6 +17,8 @@ class FoodAddPage extends StatefulWidget {
 class _FoodAddPageState extends State<FoodAddPage> {
   TextEditingController kcalController = TextEditingController();
   TextEditingController memoController = TextEditingController();
+
+  Food get food => widget.food;
 
   @override
   Widget build(BuildContext context) {
@@ -32,7 +36,9 @@ class _FoodAddPageState extends State<FoodAddPage> {
           itemBuilder: (ctx, idx) {
             if (idx == 0) {
               return Container(
-                child: Text("오늘 어떤 음식을 드셔싸요?"),
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
+                child: Text("오늘 어떤 음식을 드셨어요?", style: TextStyle(fontSize: 20)),
               );
             } else if (idx == 1) {
               return Container(
@@ -52,10 +58,54 @@ class _FoodAddPageState extends State<FoodAddPage> {
                   ),
                 ],
               ));
-            }else if(idx==2){
+            } else if (idx == 2) {
               return Container(
-                child:Container(),
-              )
+                child: Container(
+                  child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: InkWell(
+                          child: AspectRatio(
+                              child: food.image.isEmpty
+                                  ? Image.asset("assets/img/rice.png")
+                                  : AssetThumb(
+                                      asset:
+                                          Asset(food.image, "food.png", 0, 0),
+                                      width: 300,
+                                      height: 300),
+                              aspectRatio: 1 / 1))),
+                ),
+              );
+            } else if (idx == 3) {
+              return Container(
+                child: CupertinoSegmentedControl(
+                  children: {
+                    0: Text("아침"),
+                    1: Text("점심"),
+                    2: Text("저녁"),
+                    3: Text("간식"),
+                  },
+                  onValueChanged: (idx) {
+                    setState(() {
+                      food.type = idx;
+                      print(idx);
+                    });
+                  },
+                  groupValue: food.type,
+                ),
+              );
+            } else if (idx == 4) {
+              return Container(
+                  child: Column(
+                children: [
+                  Text("메모"),
+                  TextField(
+                      maxLines: 10,
+                      minLines: 10,
+                      keyboardType: TextInputType.multiline,
+                      controller: memoController,
+                      decoration: InputDecoration(border: OutlineInputBorder()))
+                ],
+              ));
             }
             return Container();
           },
